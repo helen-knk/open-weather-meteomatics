cb2f5f719205cb05bb997e84963056da8f60ecf2
Helen <helen.christine.kds@gmail.com>
2025-11-15 14:41:50 -0300

    Define o banco para MySQL e define a lib PyMySQL como ponte de conexão

--- core/settings.py
+++ core/settings.py
@@ -51,8 +51,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
