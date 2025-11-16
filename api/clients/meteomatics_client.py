b19ae9a1bc9ccdc1612446f759ff8dcdce8d3165
Helen <helen.christine.kds@gmail.com>
2025-11-16 10:18:49 -0300

    Refatora a função que realiza a busca de dados climáticos da API Meteomatics, com a finalidade de deixá-la dinâmica

--- api/clients/meteomatics_client.py
+++ api/clients/meteomatics_client.py
@@ -1,10 +1,37 @@
 import requests
 from decouple import config
+import datetime
 
-def buscar_dados_climaticos_da_api():
+def buscar_dados_climaticos_da_api(
+        latitude=-26.3044898,
+        longitude=-48.8486726,
+        data=None,
+        parametros=None
+):
+    """
+        Retorna dados climáticos da Meteomatics API.
+
+        Por padrão, retorna dados de Joinville (latitude=-26.3044898, longitude=-48.8486726).
+        Pode passar outras coordenadas se necessário.
+    """
     login = config('USER_METEOMATICS_API')
     senha = config('PASSWORD_METEOMATICS_API')
-    url = 'https://api.meteomatics.com/2025-11-14T21:45:00.000-03:00--2025-11-15T21:45:00.000-03:00/t_2m:C,precip_1h:mm,wind_speed_10m:ms/-26.3044898,-48.8486726/json'
+
+    if data is None:
+        data = datetime.date.today()
+    start = f'{data}T00:00:00.000-03:00'
+    end = f'{data}T23:59:59.000-03:00'
+    data_range = f'{start}--{end}/'
+
+    if parametros is None:
+        parametros = ['t_2m:C', 'precip_1h:mm', 'wind_speed_10m:ms']
+    parametros_str = ','.join(parametros) + '/'
+
+    coordenadas = f'{latitude},{longitude}/'
+
+    tipo = 'json'
+
+    url = f'https://api.meteomatics.com/{data_range}{parametros_str}{coordenadas}{tipo}'
 
     try:
         response = requests.get(url, auth=(login, senha))
