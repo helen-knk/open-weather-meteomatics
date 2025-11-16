3ce56577ba762713ae1228d4dd4e02cd5d0ce94d
Helen <helen.christine.kds@gmail.com>
2025-11-16 13:04:28 -0300

    Realiza a implementação do Swagger

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.swagger import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
