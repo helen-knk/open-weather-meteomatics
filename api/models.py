d6f72b59732d2511465e4bd2fb0f3a670e99b7b6
Helen <helen.christine.kds@gmail.com>
2025-11-17 11:54:44 -0300

    Adiciona o Model de ClimaDados, para armazenar os dados climáticos

--- api/models.py
+++ api/models.py
@@ -10,3 +10,10 @@ class Clima(models.Model):
 
     def __str__(self):
         return f'Clima - {self.data_criacao}'
+
+class ClimaDados(models.Model):
+    clima = models.ForeignKey('Clima', on_delete=models.CASCADE, related_name='dados')
+    data_medicao = models.DateTimeField()
+    temperatura = models.FloatField(null=True, blank=True)
+    precipitacao = models.FloatField(null=True, blank=True)
+    vento = models.FloatField(null=True, blank=True)
